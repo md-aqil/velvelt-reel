12df45a28bd0de473b17a2846a38a827192e7780
md-aqil <aqilali381@gmail.com>
2026-08-21 21:29:30 +0530

    Clean theme: remove node_modules, test files, and unnecessary files

--- scratch/check-categories.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('../../../wp-load.php');
-$terms = get_terms(array(
-    'taxonomy' => 'advertisement_category',
-    'hide_empty' => false,
-));
-foreach ($terms as $term) {
-    echo $term->name . " (" . $term->slug . ")\n";
-}
